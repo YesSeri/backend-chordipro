b6e4f7f5b3b3ab58f8d44a5c0856074eec622056
YesSeri <34012616+YesSeri@users.noreply.github.com>
2021-12-12 22:06:58 +0100

    start script

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const { test } = require('./db')
 
+const express = require('express');
+const app = express();
+// var cors = require('cors');
+
+// app.use(
+// 	cors({
+// 		credentials: true,
+// 		origin: true
+// 	})
+// );
+// app.options('*', cors());
+
+app.get('/', (req, res) => res.send('Working!!!'));
+
+app.listen(process.env.PORT || 3000, function () {
+	console.log('server running on port 3000', '');
+});
+
 async function main() {
 	const res = await test();
 	console.log({ res })
 
 }
-main()
