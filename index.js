fc155a92047cefc5bba1c48fd0cf68b8e1456374
YesSeri <34012616+YesSeri@users.noreply.github.com>
2021-12-13 18:03:33 +0100

    minimal

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const { db } = require('./db')
+// const { db } = require('./db')
 
-const express = require('express');
-const app = express();
-// app.use(function (req, res, next) {
-// 	next()
-// })
+// const express = require('express');
+// const app = express();
+// // app.use(function (req, res, next) {
+// // 	next()
+// // })
 
-app.get('/all', async (req, res) => {
-	db.getAll().then(json => res.json(json)).catch(err => {
-		console.error(err)
-		res.json("")
-	});
-});
+// app.get('/all', async (req, res) => {
+// 	db.getAll().then(json => res.json(json)).catch(err => {
+// 		console.error(err)
+// 		res.json("")
+// 	});
+// });
 app.get('/', async (req, res) => {
-	db.getTitles().then(json => res.json(json)).catch(err => {
-		console.error(err)
-		res.json("")
-	});
+	// db.getTitles().then(json => res.json(json)).catch(err => {
+	// 	console.error(err)
+	// 	res.json("")
+	// });
+	res.json("aaaaa")
 });
-app.get('/song/:id', async (req, res) => {
-	const { params: { id } } = req
-	// Send song if there is one if there is no song, send empty string.
-	// db.getObjById(id).then(song => res.json(song)).catch(err => {
-	// 	res.json({ title: '' }
-	// }))
-	db.getObjById().then(json => res.json(json)).catch(err => {
-		console.error(err)
-		res.json("")
-	});
-})
+// app.get('/song/:id', async (req, res) => {
+// 	const { params: { id } } = req
+// 	// Send song if there is one if there is no song, send empty string.
+// 	// db.getObjById(id).then(song => res.json(song)).catch(err => {
+// 	// 	res.json({ title: '' }
+// 	// }))
+// 	db.getObjById().then(json => res.json(json)).catch(err => {
+// 		console.error(err)
+// 		res.json("")
+// 	});
+// })
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
