51364b4b1169a7b87e032cc26378ab1bb5e71b55
YesSeri <34012616+YesSeri@users.noreply.github.com>
2021-12-13 10:45:16 +0100

    single song api

--- index.js
+++ index.js
@@ -12,18 +12,19 @@ const app = express();
 // );
 // app.options('*', cors());
 db.connect();
+// app.use(function (req, res, next) {
+// 	next()
+// })
 app.get('/all', async (req, res) => {
-	const json = await db.getTitles();
+	const json = await db.getAll();
 	res.json(json)
 });
 app.get('/', async (req, res) => {
 	const json = await db.getTitles();
-	res.json({ test: "test" })
-	// res.json(json)
+	res.json(json)
 });
-
-
-const port = process.env.PORT || 3000
-app.listen(port, function () {
-	console.log('Server running on: ' + port);
+app.get('/song/:id', async (req, res) => {
+	const { params: { id } } = req
+	// Send song if there is one if there is no song, send empty string.
+	db.getObjById(id).then(song => res.json(song)).catch(res.json({ title: '' }))
 });
