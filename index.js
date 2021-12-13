fe212e3373dbb244de2918c669d951f1799317d9
YesSeri <34012616+YesSeri@users.noreply.github.com>
2021-12-13 18:58:11 +0100

    added require

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { db } = require('./db')
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 // // app.use(function (req, res, next) {
 // // 	next()
